2a367c833e2a9c12958fdb8901ca4ba454e7b802
shenwuyue0000 <92183575+shenwuyue0000@users.noreply.github.com>
2025-02-20 19:36:29 +0100

    Update consumer.py

--- kafka/consumer.py
+++ kafka/consumer.py
@@ -10,7 +10,7 @@ local_path = 'D:/hadoop/movies_data-2.json'
 consumer = KafkaConsumer(
     KAFKA_TOPIC,
     bootstrap_servers=KAFKA_BROKER,
-    auto_offset_reset='earliest',  # 从最早的消息开始消费
+    auto_offset_reset='earliest',  # Commencez à consommer dès le premier message
     enable_auto_commit=True,
     value_deserializer=lambda m: json.loads(m.decode('utf-8')),
     api_version=(2, 7, 0)
@@ -23,7 +23,7 @@ with open(local_path, 'w', encoding='utf-8') as file:
         movie = message.value
         try:
             file.write(json.dumps(movie) + "\n")
-            file.flush()  # 确保每条消息立即写入磁盘
+            file.flush()  # Garantit que chaque message est écrit immédiatement sur le disque
             print(f"💾 Successfully saved: {movie['title']}")
         except Exception as e:
             print(f"❌ Failed to save {movie['title']}: {e}")
